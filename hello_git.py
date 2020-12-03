0c80e5d32339b1b30f4d18a200e9bdad17995c1b
ly <you@qq.com>
2020-12-03 16:26:09 +0800

    原始版本

---
+++ hello_git.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-12-03 12:06:43
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+
+print("hellow git")
